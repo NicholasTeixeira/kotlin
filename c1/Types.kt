90dc9e22e35836b8133a3cc6dc950e6efe5b5503
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-28 18:09:16 -0300

    Create Types.kt

---
+++ c1/Types.kt
@@ -0,0 +1,35 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c1
+
+fun main() {
+/*
+Type   Bit
+Double 64
+Float  32
+Long   64
+Int    32
+Short  16
+Byte    8
+Boolean ?
+String  ?
+Char    ?
+ */
+    println("Double MAX ${Double.MAX_VALUE} - MIN ${Double.MIN_VALUE}")
+    println("Float MAX ${Float.MAX_VALUE} - MIN ${Float.MIN_VALUE}")
+    println("Long MAX ${Long.MAX_VALUE} - MIN ${Long.MIN_VALUE}")
+    println("Int MAX ${Int.MAX_VALUE} - MIN ${Int.MIN_VALUE}")
+    println("Short MAX ${Short.MAX_VALUE} - MIN ${Short.MIN_VALUE}")
+    println("Byte MAX ${Byte.MAX_VALUE} - MIN ${Byte.MIN_VALUE}")
+
+    var c: Char = 'M'
+    var s: String = "Qualquer tamanho necessário"
+    var b: Boolean = true
+    var double: Double =  10.00000000000200000000000
+    var float: Float = 10f
+    var long: Long = 10L
+    var int: Int = 10
+    var short: Short = 10
+    var byte: Byte = 10
+
+    var idade: Int = 25
+
+}

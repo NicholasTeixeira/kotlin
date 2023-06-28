3cdd96b28b794c4f0494de26754b72ceca945dc6
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-28 18:09:13 -0300

    Create Main.kt

---
+++ c1/Main.kt
@@ -0,0 +1,9 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c1
+
+fun main() {
+    println("Olá Mundo!!")
+//println é pra mostrar a linha
+/* esse é um comentário de varias linhas eu acho
+e posso estar certo então.
+ */
+}

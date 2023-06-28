f921d2e8077d8ab11bca5a4e324b08912e5af33d
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-28 18:09:25 -0300

    Create ConversaoDeTipo.kt

---
+++ c2/FirstSteps/ConversaoDeTipo.kt
@@ -0,0 +1,13 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c2.FirstSteps
+
+fun main() {
+    val i1: Int = 10
+    val d1 = i1.toDouble()
+    println(d1)
+
+    val l1 = i1.toLong()
+    println(l1)
+
+    val d2 = d1.toInt()
+    println(d2)
+}

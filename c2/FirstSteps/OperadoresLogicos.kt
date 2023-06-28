8d3e60416619e53f6064af9f5c11ea145536d7de
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-28 18:42:43 -0300

    Criação de uma tabela verdade (AND, OR, NOT)

---
+++ c2/FirstSteps/OperadoresLogicos.kt
@@ -0,0 +1,26 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c2.FirstSteps
+
+fun main() {
+
+    //AND
+    println("T AND T ${true && true}")
+    println("T AND F ${true && false}")
+    println("F AND T ${false && true}")
+    println("F AND F ${false && false}")
+
+    println()
+
+    //OR
+    println("T OR T ${true || true}")
+    println("T OR F ${true || false}")
+    println("F OR T ${false || true}")
+    println("F OR F ${false || false}")
+
+    println()
+
+    //NOT
+    println("NOT T ${!true}")
+    println("NOT F ${!false}")
+
+
+}

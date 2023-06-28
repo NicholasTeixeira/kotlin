31af3b145c7a380d32afd43d5ad9b7c96ed2f60c
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-28 18:09:27 -0300

    Create imc.kt

---
+++ c2/FirstSteps/imc.kt
@@ -0,0 +1,27 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c2.FirstSteps
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Seja bem vindo ao calculo de IMC desenvolvido por CodeSmithSyd")
+    print("Poderia me informar seu nome? -> ")
+    val name = readln()
+
+    print("Ok $name, agora pode me dizer sua idade? -> ")
+    val idade = scanner.nextInt()
+
+    print("Qual o seu Peso? -> ")
+    val peso = scanner.nextFloat()
+
+    print("Qual a sua Altura? -> ")
+    val altura = scanner.nextFloat()
+
+    val altxalt = altura * altura
+    val imc = peso + altxalt
+    val imcr = imc.toInt()
+
+    println("""Bem $name!
+        |Sua idade é $idade!  
+        |Seu IMC = $imcr!""".trimMargin())
+}

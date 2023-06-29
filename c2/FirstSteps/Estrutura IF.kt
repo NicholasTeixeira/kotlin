fc36093c45b9ad34dcde62922ed25f159c78b11d
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-29 14:10:17 -0300

    Alterações nas atividades

---
+++ c2/FirstSteps/Estrutura IF.kt
@@ -0,0 +1,16 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c2.FirstSteps
+
+import java.util.Scanner
+
+val scanner = Scanner(System.`in`)
+
+fun main() {
+    print("Qual a temperatura hoje?")
+    val temp = scanner.nextInt()
+
+    if (temp > 20){
+        println("Está calor")
+    }
+    else
+        println("Está fresquinho")
+}
---
+++ out/production/kotlin/curso/kotlin/devsyd/estudoskotlin/kotlin/README.md
@@ -0,0 +1,2 @@
+# kotlin
+ Repositorio destinado a armazenar e compartilhar meu processo de aprendizado e especialização na linguagem Kotlin, voltado para desenvolvimento mobile.

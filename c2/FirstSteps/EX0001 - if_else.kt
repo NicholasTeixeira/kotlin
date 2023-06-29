eb0efd8f1a809167c287fafea3ce3ca5bb9169f3
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-06-29 14:30:34 -0300

    Estudos a respeito de IF e ELSE

---
+++ c2/FirstSteps/EX0001 - if_else.kt
@@ -0,0 +1,25 @@
+package curso.kotlin.devsyd.estudoskotlin.kotlin.c2.FirstSteps
+
+import java.util.Scanner
+
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Bom dia, digite aqui seu nome: ")
+    val name = readln()
+
+    println("Ok $name, vamos praticar ir e else então")
+
+    print("Digite um numero: ")
+    var n1 = scanner.nextInt()
+
+
+    if (n1 > 10){
+        n1 = 10
+        println("O valor informado não era igual ou menor q 10, alterei ele e agora é: $n1!!")
+    }
+    val n1xn1 = n1 * n1
+    println("Resultado $n1xn1")
+    println("Obrigado")
+}

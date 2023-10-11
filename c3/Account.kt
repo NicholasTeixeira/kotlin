0b3d68992ae0a4bc94c6abac1697d61f5e68725d
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-10-10 22:42:53 -0300

    .

--- c3/Account.kt
+++ c3/Account.kt
@@ -1,6 +1,12 @@
 package curso.kotlin.devsyd.estudoskotlin.kotlin.c3
 
+import java.time.temporal.TemporalAmount
+
 class Account {
 
     var balance: Double = 0.0
+
+    fun deposit(amount: Double) {
+        balance += amount
+    }
 }
--- c3/App2.kt
+++ c3/App2.kt
@@ -5,11 +5,10 @@ fun main() {
     val a1 = Account()
     val a2 = Account()
 
-    a1.balance = 100.0
-    a2.balance = 1_000_000.000
+    a1.deposit(2000.0)
+    a2.deposit(150.0)
 
     println(a1.balance)
     println(a2.balance)
-
 
 }

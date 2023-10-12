6eb9b8751967c63e7c79c299c115f338a5015b10
Nicholas Teixeira <91445534+NicholasTeixeira@users.noreply.github.com>
2023-10-12 15:11:33 -0300

    .

--- c3/App3.kt
+++ c3/App3.kt
@@ -5,8 +5,8 @@ fun main() {
     val a1 = Account("1234-001", "Nicholas Diogo Teixeira de Jesus Silva")
     val a2 = Account("1234-002", "Raabe Bitencourt de Jesus Teixeira")
 
-    a1.deposit(100.00)
-    a2.deposit(1000000.00)
+    a1.deposit(100)
+    a2.deposit(1000000)
 
     a1.print()
     a2.print()
